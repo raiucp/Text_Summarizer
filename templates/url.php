e04747d66b6514be927a1de0e3be748d06514926
raiucp <nidhijangir1684@gmail.com>
2018-07-10 08:26:34 -0700

    Add files via upload

---
+++ templates/url.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>TexT SummarizeR.</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		<!-- Custom styles for this template -->
+		<link href="\static\navbar.css" rel="stylesheet">
+	</head>
+	<body background="static\pic4.jpg" style="opacity:0.8;" >
+	<div  class="container jumbotron" >
+	   	<h4>{{text}}</h4>
+	</div>
+	
+	</body>
+</html>
